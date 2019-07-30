65e8a40250c423c02722c23f177b4bcb70d3af5e
Hmmambk1994 <bhmmam@gmail.com>
2019-07-30 17:30:33 +0200

    first push

--- myCode.js
+++ myCode.js
@@ -1,28 +1,47 @@
-
-
-$("#run").click(function(){
-  var one=$("#male").val().trim();
-  var two=$("#female").val().trim();
-
-  var num=0;
-  if((one==="عبد الرحمن" && two ==="نور"  ||one==="نور" && two === "عبد الرحمن")||
-  (one==="محمود" && two ==="رنيم"  ||one==="رنيم" && two ==="محمود"   )||
-  (one ==="fares" && two ==="nadja" ||one ==="nadja" && two ==="fares")||
-  (one ==="قمر" && two ==="محمد" ||one ==="محمد" && two ==="قمر")){
-    num=100;
-  }else {
-    num=Math.floor(Math.random() * 50)+50;
+let measure = (one, two) => {
+  let num = 0;
+  if (
+    ((one === "عبد الرحمن" && two === "نور") ||(one === "نور" && two === "عبد الرحمن")) ||
+    ((one === "محمود" && two === "رنيم") ||(one === "رنيم" && two === "محمود")) ||
+    ((one === "fares" && two === "monika") ||(one === "monika" && two === "fares")) ||
+    ((one === "قمر" && two === "محمد") || (one === "محمد" && two === "قمر"))
+  ) {
+    num = 100;
+  } else {
+    num = Math.floor(Math.random() * 50) + 50;
   }
 
-  var interval =setInterval(function() {
-    var temp=Math.floor(Math.random()*100);
-    $("#meter").text(temp+"%");
-  },50)
+  return num;
+};
 
-  setTimeout(function () {
+
+let measurmentShow=(measureNum)=>{
+
+  let interval=setInterval(()=>{
+    let temp = Math.floor(Math.random * 100);
+    document.querySelector("#meter").innerHTML=temp+"%";
+  },50);
+
+  setTimeout(() => {
     clearInterval(interval);
-    $("#meter").text(num+"%");
-  },1500)
+    document.querySelector("#meter").innerHTML=measureNum+"%";
+  }, 1500);
+
+}
 
 
-})
+const app = () => {
+  document.querySelector("#meter").innerHTML = 0 + "%";
+
+  let begin = document.querySelector("#begin");
+  begin.addEventListener("click", () => {});
+  let one = document.querySelector("#one");
+  let two = document.querySelector("#two");
+  one.trim().toLowerCase();
+  two.trim().toLowerCase();
+
+  let measureNum=measure(one,two);
+  measurmentShow(measureNum);
+};
+
+app();
