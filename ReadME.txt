ea46db4faea5dac65ed973b54092e8a5c488c653
Hmmambk1994 <bhmmam@gmail.com>
2019-08-01 18:36:29 +0200

    the final commit (AR version)

---
+++ ReadME.txt
@@ -0,0 +1,2 @@
+loveland is a randomly love measurement made mainly for programming 
+practicing and fun
--- myCode.js
+++ myCode.js
@@ -1,47 +1,55 @@
 let measure = (one, two) => {
+  // it's a made up result for my realtives 
+  // otherwise it'll return a random result 
   let num = 0;
   if (
-    ((one === "عبد الرحمن" && two === "نور") ||(one === "نور" && two === "عبد الرحمن")) ||
-    ((one === "محمود" && two === "رنيم") ||(one === "رنيم" && two === "محمود")) ||
-    ((one === "fares" && two === "monika") ||(one === "monika" && two === "fares")) ||
-    ((one === "قمر" && two === "محمد") || (one === "محمد" && two === "قمر"))
+    ((one === "عبد الرحمن" && two === "نور") ||
+      (one === "نور" && two === "عبد الرحمن")) ||
+    ((one === "محمود" && two === "رنيم") ||
+      (one === "رنيم" && two === "محمود")) ||
+    ((one === "fares" && two === "monika") ||
+      (one === "monika" && two === "fares")) ||
+    ((one === "قمر" && two === "محمد") ||
+      (one === "محمد" && two === "قمر"))
   ) {
     num = 100;
   } else {
     num = Math.floor(Math.random() * 50) + 50;
   }
-
   return num;
 };
 
 
-let measurmentShow=(measureNum)=>{
-
-  let interval=setInterval(()=>{
-    let temp = Math.floor(Math.random * 100);
-    document.querySelector("#meter").innerHTML=temp+"%";
-  },50);
+let measurmentShow = (measureNum) => {
+  //will show a number on the result field each 0.05 of a second as it's measureing before the measureNum is passed into the result field 
+  let interval = setInterval(function() {
+    let temp = Math.floor(Math.random() * 100);
+    document.querySelector("#meter").innerHTML = temp + "%";
+  }, 50);
 
   setTimeout(() => {
     clearInterval(interval);
-    document.querySelector("#meter").innerHTML=measureNum+"%";
-  }, 1500);
+    document.querySelector("#meter").innerHTML = measureNum + "%";
+  }, 1000);
+};
 
-}
-
+let operate = () => {
+  //will be sure the none of the inputs are empty
+  let one = document.querySelector("#one").value;
+  let two = document.querySelector("#two").value;
+  one.toLowerCase().trim();
+  two.toLowerCase().trim();
+  if (one === "" || two === "") {
+    alert("املا البيانات في خانات الكتابة! من قضلك");
+    return;
+  }
+  
+  let measureNum = measure(one,two);
+  measurmentShow(measureNum);
+};
 
 const app = () => {
   document.querySelector("#meter").innerHTML = 0 + "%";
-
-  let begin = document.querySelector("#begin");
-  begin.addEventListener("click", () => {});
-  let one = document.querySelector("#one");
-  let two = document.querySelector("#two");
-  one.trim().toLowerCase();
-  two.trim().toLowerCase();
-
-  let measureNum=measure(one,two);
-  measurmentShow(measureNum);
 };
 
 app();
