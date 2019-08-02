acaa0c09f0432d8f9ec855684c7d0b030f366b58
Hmmambk1994 <bhmmam@gmail.com>
2019-08-02 16:23:08 +0200

    tolowercase doesn't work

--- myCode.js
+++ myCode.js
@@ -1,16 +1,19 @@
 let measure = (one, two) => {
   // it's a made up result for my friends
-  // otherwise it'll return a random result 
+  // otherwise it'll return a random result
+  one.trim().toLowerCase();
+  two.trim().toLowerCase();
+  alert(one);
+  alert(two);
   let num = 0;
   if (
-    ((one === "عبد الرحمن" && two === "نور") ||
-      (one === "نور" && two === "عبد الرحمن")) ||
+    (one === "عبد الرحمن" && two === "نور") ||
+    (one === "نور" && two === "عبد الرحمن") ||
     ((one === "محمود" && two === "رنيم") ||
       (one === "رنيم" && two === "محمود")) ||
     ((one === "fares" && two === "monika") ||
       (one === "monika" && two === "fares")) ||
-    ((one === "قمر" && two === "محمد") ||
-      (one === "محمد" && two === "قمر"))
+    ((one === "قمر" && two === "محمد") || (one === "محمد" && two === "قمر"))
   ) {
     num = 100;
   } else {
@@ -19,9 +22,8 @@ let measure = (one, two) => {
   return num;
 };
 
-
-let measurmentShow = (measureNum) => {
-  //will show a number on the result field each 0.05 of a second as it's measureing before the measureNum is passed into the result field 
+let measurmentShow = measureNum => {
+  //will show a number on the result field each 0.05 of a second as it's measureing before the measureNum is passed into the result field
   let interval = setInterval(function() {
     let temp = Math.floor(Math.random() * 100);
     document.querySelector("#meter").innerHTML = temp + "%";
@@ -37,16 +39,13 @@ let operate = () => {
   //will be sure the none of the inputs are empty
   let one = document.querySelector("#one").value;
   let two = document.querySelector("#two").value;
-  one.trim().toLowerCase();
-  two.trim().toLowerCase();
-  alert(one);
-  alert(two);
+
   if (one === "" || two === "") {
     alert("املا البيانات في خانات الكتابة! من قضلك");
     return;
   }
-  
-  let measureNum = measure(one,two);
+
+  let measureNum = measure(one, two);
   measurmentShow(measureNum);
 };
 
